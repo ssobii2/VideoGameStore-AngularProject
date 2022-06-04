be45940bc368b9790771c618757dff9fce7e778e
THUNDER-PC\subha <rntvtf@pte.hu>
2022-06-04 19:38:09 +0200

    made products page

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import Games from '../../../assets/products.json';
+
+export interface PRODUCT {
+  productId: number;
+  productCode: string;
+  productName: string;
+  productTitle: string;
+  productSubTitle: string;
+  productDescription: string;
+  productPlatform: string;
+  productSmallImage: string;
+  productPrice: number;
+}
 
 @Component({
   selector: 'app-products',
@@ -6,6 +19,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+
+  public Product: PRODUCT[] = Games;
 
   constructor() { }
 
