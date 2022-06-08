a16e421f9750f3df0daf008697fc26fdb46fef04
THUNDER-PC\subha <rntvtf@pte.hu>
2022-06-08 18:54:15 +0200

    made products backend

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PRODUCT } from 'src/models/product.model';
+import { ProductsService } from 'src/services/products.service';
 import Games from '../../../assets/products.json';
 
-export interface PRODUCT {
-  productId: number;
-  productCode: string;
-  productName: string;
-  productTitle: string;
-  productSubTitle: string;
-  productDescription: string;
-  productPlatform: string;
-  productSmallImage: string;
-  productPrice: number;
-}
+// export interface PRODUCT {
+//   productId: number;
+//   productCode: string;
+//   productName: string;
+//   productTitle: string;
+//   productSubTitle: string;
+//   productDescription: string;
+//   productPlatform: string;
+//   productSmallImage: string;
+//   productPrice: number;
+// }
 
 @Component({
   selector: 'app-products',
@@ -20,11 +22,16 @@ export interface PRODUCT {
 })
 export class ProductsComponent implements OnInit {
 
-  public Product: PRODUCT[] = Games;
+  public Product: PRODUCT[] = [];
 
-  constructor() { }
+  constructor(
+    private productService: ProductsService,
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.Product = data;
+    });
   }
 
 }
---
+++ src/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface PRODUCT {
+  productId: number;
+  productCode: string;
+  productName: string;
+  productTitle: string;
+  productSubTitle: string;
+  productDescription: string;
+  productPlatform: string;
+  productSmallImage: string;
+  productPrice: number;
+}
